48946902a4547cf0bd0485a9b88907f56506c81f
Avrumi Friedman <a.friedman07@gmail.com>
2021-03-02 23:49:13 -0500

    Add project files.

---
+++ ListPeoplePost.Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using ListPeoplePost.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ListPeoplePost.Data;
+
+namespace ListPeoplePost.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString =
+            "Data Source=.\\sqlexpress;Initial Catalog=OneToManyDemo;Integrated Security=true;";
+
+
+        public IActionResult Index()
+        {
+            var mgr = new PersonManager(_connectionString);
+            var vm = new HomePageViewModel
+            {
+                People = mgr.GetPeople(),
+            };
+            if (TempData["success-message"] != null)
+            {
+                vm.Message = (string)TempData["success-message"];
+            }
+            return View(vm);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(IEnumerable<Person> people)
+        {
+            var mgr = new PersonManager(_connectionString);
+            mgr.AddPeople(people.Where(p => !String.IsNullOrEmpty(p.FirstName) && !String.IsNullOrEmpty(p.LastName)).ToList());
+
+            TempData["success-message"] = "People added successfully!";
+
+            return Redirect("/home/index");
+        }
+    }
+}
